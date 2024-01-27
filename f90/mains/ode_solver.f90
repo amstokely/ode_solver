7c345bfa57765b2ee787f20fc25b3308d01ed871
astokely <astokely@mmm-alta.mmm.ucar.edu>
2024-01-27 15:29:57 -0700

    feat: beta, rho, and sigma and now initialized using the abstract command line setter interface.

--- f90/mains/ode_solver.f90
+++ f90/mains/ode_solver.f90
@@ -1,35 +1,42 @@
 program ode_solver
     use equations, only : set_initial_state, f
     use observer, only : observer_init, observer_finalize, observer_write
-    use lorenz_system, only : x0, y0, z0, lorenz, set_x0, set_y0, set_z0
+    use lorenz_system, only : x0, y0, z0, lorenz, set_x0, set_y0, set_z0, set_sigma, set_rho, set_beta, sigma, rho, beta
     use ode_system, only : system_size, system
     use command_line_setters
     use euler, only : euler_step
     use rk, only: rk2
     implicit none
 
-    character(len = 100) :: dt_arg, n_arg, output_file, x0_arg, y0_arg, z0_arg
+    character(len = 100) :: dt_arg, n_arg, output_file, x0_arg, y0_arg, z0_arg, sigma_arg, rho_arg, beta_arg
     integer :: n
     real :: dt, t
     real, dimension(:), allocatable :: s
     procedure(f), pointer :: fn
     !
-    type(setter_ptr), dimension(3) :: setters
+    allocate(setters(6))
     setters(1)%ptr => set_x0
     setters(2)%ptr => set_y0
     setters(3)%ptr => set_z0
+    setters(4)%ptr => set_sigma
+    setters(5)%ptr => set_rho
+    setters(6)%ptr => set_beta
     fn => f
 
 
+    ! TODO: Add this behavior to a seperate module that will generically parse and set parameters from the command line
     x0_arg = "10.0"
     y0_arg = "0.0"
     z0_arg = "10.0"
+    sigma_arg = "10.0"
+    rho_arg = "28.0"
+    beta_arg = "2.66667"
     call setters(1)%ptr(x0_arg)
     call setters(2)%ptr(y0_arg)
     call setters(3)%ptr(z0_arg)
-    print *, "x0 = ", x0
-    print *, "y0 = ", y0
-    print *, "z0 = ", z0
+    call setters(4)%ptr(sigma_arg)
+    call setters(5)%ptr(rho_arg)
+    call setters(6)%ptr(beta_arg)
     call get_command_argument(1, dt_arg)
     call get_command_argument(2, n_arg)
     call get_command_argument(3, output_file)
--- f90/src/lorenz_system.f90
+++ f90/src/lorenz_system.f90
@@ -8,7 +8,7 @@ module lorenz_system
     implicit none
 
     private
-    public :: lorenz, sigma, rho, beta, x0, y0, z0, set_x0, set_y0, set_z0
+    public :: lorenz, sigma, rho, beta, x0, y0, z0, set_x0, set_y0, set_z0, set_sigma, set_rho, set_beta
 
     real :: sigma = 10.0, rho = 28.0, beta = 8.0/3.0, x0, y0, z0
 
@@ -30,6 +30,24 @@ contains
         character(len=*), intent(in) :: z0_arg
         read(z0_arg, *) z0
     end subroutine set_z0
+
+    subroutine set_sigma(sigma_arg)
+        implicit none
+        character(len=*), intent(in) :: sigma_arg
+        read(sigma_arg, *) sigma
+    end subroutine set_sigma
+
+    subroutine set_rho(rho_arg)
+        implicit none
+        character(len=*), intent(in) :: rho_arg
+        read(rho_arg, *) rho
+    end subroutine set_rho
+
+    subroutine set_beta(beta_arg)
+        implicit none
+        character(len=*), intent(in) :: beta_arg
+        read(beta_arg, *) beta
+    end subroutine set_beta
     !/*
     ! Name: lorenz_dx
     !
